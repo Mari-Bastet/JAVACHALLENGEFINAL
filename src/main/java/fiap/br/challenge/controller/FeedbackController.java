5b2e3ee1690e1144ba4fcd1a89aa02c6dd0d45c1
bastet-mari <marianabastos7@hotmail.com>
2024-05-19 15:44:31 -0300

    Ajuste documentação

--- src/main/java/fiap/br/challenge/controller/FeedbackController.java
+++ src/main/java/fiap/br/challenge/controller/FeedbackController.java
@@ -2,10 +2,13 @@ package fiap.br.challenge.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,34 +20,30 @@ import fiap.br.challenge.repository.FeedbackRepository;
 @RequestMapping("/feedbacks")
 public class FeedbackController {
 
-	
-	
 	@Autowired
 	private FeedbackRepository repo;
 	
-	
-	 @RequestMapping(value="/{id}",method = RequestMethod.GET)
-		public ResponseEntity<?> listarFeedbacksPorIdCliente(@PathVariable Long id){
-	    	
-	    	try {
-	    		List<Feedback> feedbacks =  repo.findByClienteId(id);
-	    		if(feedbacks.isEmpty()) {
-                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Feedbacks do cliente " + id + " não encontrados");
+	Logger logger = LoggerFactory.getLogger(FeedbackController.class);
 
-    	        	
-    	        }else {
-                    return ResponseEntity.ok(feedbacks);
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> listarFeedbacksPorIdCliente(@PathVariable Long id) {
 
-                }
+		try {
+			List<Feedback> feedbacks = repo.findByClienteId(id);
+			if (feedbacks.isEmpty()) {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND)
+						.body("Feedbacks do cliente " + id + " não encontrados");
 
-    	}catch(Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao listar feedbacks do cliente: " + e.getMessage());
-    	}
-	    	
-	    	
-	    	
-	    }
+			} else {
+				return ResponseEntity.ok(feedbacks);
 
+			}
 
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Erro ao listar feedbacks do cliente: " + e.getMessage());
+		}
+
+	}
 
 }
