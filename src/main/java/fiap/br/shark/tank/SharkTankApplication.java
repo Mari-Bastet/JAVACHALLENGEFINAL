c739e8129d6052205448389c0b0116f3ca6d5d26
bastet-mari <marianabastos7@hotmail.com>
2024-06-02 14:49:04 -0300

    limpando tudo

--- src/main/java/fiap/br/shark/tank/SharkTankApplication.java
+++ src/main/java/fiap/br/shark/tank/SharkTankApplication.java
@@ -1,4 +1,4 @@
-package fiap.br.challenge;
+package fiap.br.shark.tank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +8,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 //@EntityScan("fiap.br.challenge.teste")
 //@EnableJpaRepositories("fiap.br.challenge.teste")
-public class ChallengeApplication {
+public class SharkTankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChallengeApplication.class, args);
+		SpringApplication.run(SharkTankApplication.class, args);
 	}
 
 }
